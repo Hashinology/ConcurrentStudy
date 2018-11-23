7ead69aa440fdc20e188b5b273ede80d5869a5bd
Hashi <m.sharif.uk@gmail.com>
2018-11-23 09:14:07 +0000

    Initial commit

---
+++ app/src/main/java/com/example/hashi/lyndaconcurrentstudy/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.hashi.lyndaconcurrentstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "CodeRunner";
+
+    // View object references
+    private ScrollView mScroll;
+    private TextView mLog;
+    private ProgressBar mProgressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mScroll = findViewById(R.id.scrollLog);
+        mLog = findViewById(R.id.tvLog);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        mLog.setText(R.string.lorem_ipsum);
+    }
+
+    public void runCode(View view) throws InterruptedException {
+        log("Running code");
+        Thread.sleep(3000);
+    }
+    //  Clear the output, called from the onClick event in the layout file
+    public void clearOutput(View v) {
+        mLog.setText("");
+        scrollTextToEnd();
+    }
+
+    //  Log output to logcat and the screen
+    private void log(String message) {
+        Log.i(TAG, message);
+        mLog.append(message + "\n");
+        scrollTextToEnd();
+    }
+
+    private void scrollTextToEnd() {
+        mScroll.post(new Runnable() {
+            @Override
+            public void run() {
+                mScroll.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+
+    private void displayProgressBar(boolean display) {
+        if (display) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+}
